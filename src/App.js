7973492e243846ef4c10238f059226e2b6c478c1
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2019-11-23 02:04:43 +0300

    task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useJsonFetch from "./hooks/useJsonFetch";
+
+function AppHook({ url }) {
+  const [data, loading, error] = useJsonFetch(url);
+  return (
+    <div className="ui segment">
+      {data && <p>{data.status}</p>}
+      {loading && <p>Loading</p>}
+      {error && <p>Has Error</p>}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ui raised very padded text container segment">
+        <div className="ui segments">
+          <AppHook url={process.env.REACT_APP_DATA_URL} />
+          <AppHook url={process.env.REACT_APP_ERROR_URL} />
+          <AppHook url={process.env.REACT_APP_LOADING_URL} />
+        </div>
+      </div>
     </div>
   );
 }
